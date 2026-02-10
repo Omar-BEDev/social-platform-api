fa3b74c5dfe6c9b4a18664ee08f2ffe3b491b0eb
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-10 20:59:46 +0000

    feat(users): make users validation

---
+++ src/modules/users/users.schema.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const passwordValidation = z.string().min(8).max(50).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#|\\@$!%*?&])[A-Za-z\d#|\\@$!%*?&]{8,}$/, 
+'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character');
+
+export const userSchema = z.object({
+  name: z.string().min(1),
+  nickname: z.string().min(1),
+  birthday: z.coerce.date(),
+  address: z.object({
+    city: z.string().optional(),
+    country: z.string().optional(),
+  }).optional(),
+  password: passwordValidation,
+  email: z.string().email(),
+  language: z.array(z.string()).max(6).optional(),
+  framework: z.array(z.string()).max(3).optional(),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: passwordValidation,
+});
