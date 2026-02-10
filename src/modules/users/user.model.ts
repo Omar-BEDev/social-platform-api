78e45a1879ca1034feafb597e934714a77d5f4cc
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-10 13:36:30 +0000

    feat(users): add user schema

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,31 @@
+import { Schema, model, InferSchemaType, Document } from 'mongoose';
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    nickname: { type: String, required: true, unique: true },
+    birthday: { type: Date, required: true },
+    address: {
+        city: { type: String },
+        country: { type: String }
+    },
+    followers: { type: Number, default: 0 },
+    following: { type: Number, default: 0 },
+    role: { type: String, required: true, default: 'user' },
+    portfolioImage: { type: String },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    language: {
+        type: [String],
+        validate: (v: string[]) => Array.isArray(v) && v.length <= 6
+    },
+    framework: {
+        type: [String],
+        validate: (v: string[]) => Array.isArray(v) && v.length <= 3
+    }
+}, { timestamps: true });
+
+export type IUser = InferSchemaType<typeof userSchema>;
+export interface IUserDocument extends IUser, Document { }
+const User = model<IUser>('User', userSchema);
+
+export default User;
