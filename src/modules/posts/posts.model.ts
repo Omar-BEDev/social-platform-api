5f7223390ab3c8aea02c9647c1f8be202a36ca7a
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-10 14:03:11 +0000

    feat(posts): make posts model

---
+++ src/modules/posts/posts.model.ts
@@ -0,0 +1,24 @@
+
+import { Schema, model, InferSchemaType, Types } from 'mongoose';
+
+const postSchema = new Schema({
+    authorId: { type: Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+    languageTag: {
+        type: [String],
+        required: true,
+        validate: (v: string[]) => Array.isArray(v) && v.length >= 1 && v.length <= 6
+    },
+    frameworkTag: {
+        type: [String],
+        validate: (v: string[]) => Array.isArray(v) && v.length >= 1 && v.length <= 3
+    },
+    likesCount: { type: Number, default: 0 },
+    groupId: { type: Types.ObjectId, default: null }
+}, { timestamps: true });
+
+type IPost = InferSchemaType<typeof postSchema>;
+
+const Post = model<IPost>('Post', postSchema);
+
+export default Post;
