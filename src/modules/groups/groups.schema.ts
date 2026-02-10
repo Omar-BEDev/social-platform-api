8966411c3b006807f118f6159dffb069c84076ac
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-10 21:22:47 +0000

    feat(groups): add validation of groups

---
+++ src/modules/groups/groups.schema.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+import { Types } from 'mongoose';
+
+export const objectIdSchema = z.string().refine((val) => {
+    return Types.ObjectId.isValid(val);
+}, {
+    message: "Invalid ObjectId",
+});
+
+export const groupParamsSchema = z.object({
+  id: objectIdSchema,
+});
+
+export const groupPostSchema = z.object({
+  authorId: objectIdSchema,
+  content: z.string().min(1).max(2000),
+  languageTag: z.array(z.string()).min(1).max(6),
+  frameworkTag: z.array(z.string()).max(3).optional(),
+  groupId: objectIdSchema,
+});
