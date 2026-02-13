3fd1e7ee651db067692ac2144aa5bb3de5140235
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-13 23:10:16 +0000

    fix(follows): fix follows controller mistakes

--- src/modules/follows/follows.controller.ts
+++ src/modules/follows/follows.controller.ts
@@ -1,9 +1,10 @@
 import { Response } from 'express';
 import { AuthRequest } from '../../utils/payload';
 import * as followService from './follows.service';
+import { ApiError } from '../../utils/ApiError';
 
 export const followUser = async (req: AuthRequest, res: Response) => {
-    if (!req.user) throw new Error('User not authenticated')
+    if (!req.user) throw new ApiError('User not authenticated',403)
     const currentUserId = req.user.id;
     const targetUserId = req.params.userId;
     const result = await followService.follow(currentUserId, targetUserId);
@@ -11,7 +12,7 @@ export const followUser = async (req: AuthRequest, res: Response) => {
 };
 
 export const unfollowUser = async (req: AuthRequest, res: Response) => {
-    if (!req.user) throw new Error('User not authenticated')
+    if (!req.user) throw new ApiError('User not authenticated',403)
     const currentUserId = req.user.id;
     const targetUserId = req.params.userId;
     const result = await followService.unfollow(currentUserId, targetUserId);
@@ -19,7 +20,7 @@ export const unfollowUser = async (req: AuthRequest, res: Response) => {
 };
 
 export const getFollows = async (req: AuthRequest, res: Response) => {
-    if (!req.user) throw new Error('User not authenticated')
+    if (!req.user) throw new ApiError('User not authenticated',403)
     const userId = req.user.id;
     const follows = await followService.followers(userId);
     res.status(200).json(follows);
