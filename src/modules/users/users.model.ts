8fc4dc5c02e4d921d8f041455786d6e30bd6e468
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-14 17:53:39 +0000

    feat(users): add pre middelware to users module

--- src/modules/users/users.model.ts
+++ src/modules/users/users.model.ts
@@ -1,5 +1,5 @@
 import { Schema, model, InferSchemaType, Document } from 'mongoose';
-
+import bcrypt from "bcrypt"
 const userSchema = new Schema({
     name: { type: String, required: true },
     nickname: { type: String, required: true },
@@ -22,7 +22,12 @@ const userSchema = new Schema({
         validate: (v: string[]) => Array.isArray(v) && v.length <= 3
     }
 });
-
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    this.password = await bcrypt.hash(this.password, 10);
+    return;
+    
+})
 export type IUser = InferSchemaType<typeof userSchema>;
 export interface IUserDocument extends IUser, Document { }
 export const User = model<IUser>('User', userSchema);
