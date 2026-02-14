f45dc8f0ae066d201cd50c589b1a17ceb569e8f0
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-14 15:17:40 +0000

    chors(api): add Api routes in app file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
 import cors from 'cors';
 import {  generalLimiter } from './middleware/rate-limit';
+import adminRoutes from './modules/admin/admin.routes';
+import userRoutes from './modules/users/users.routes';
+import postRoutes from './modules/posts/posts.routes';
+import followRoutes from './modules/follows/follows.routes';
+import groupRoutes from './modules/groups/groups.routes';
 
 dotenv.config();
 
@@ -18,5 +23,13 @@ app.use(express.json());
 
 app.use('/users/login');
 app.use(generalLimiter);
+
+//API Routes
+app.use('/api/admin',adminRoutes)
+app.use('/api/users',userRoutes)
+app.use('/api/posts',postRoutes)
+app.use('/api/follows',followRoutes)
+app.use('/api/groups',groupRoutes)
+
 
 export default app;
