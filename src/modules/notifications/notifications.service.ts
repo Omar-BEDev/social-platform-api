2f0c9c212a15ef28f33f0f384cfff9e4ff7ea302
kaliochibrahim <kaliochibrahim@gmail.com>
2026-02-15 14:39:36 +0000

    feat(notifications): add notifications modulelogic

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -1,5 +1,6 @@
 import { getIO } from "../../config/socket";
 import { catchSocketError } from "../../utils/socketCatchErr";
+import User from "../users/users.model";
 import { INotification } from "./notifications.model";
 import { Notification } from "./notifications.model";
 import { Types } from 'mongoose';
@@ -9,7 +10,7 @@ export const sendAndSaveNotifacation = async (data: INotification) => {
     const newNotification = await Notification.create(data);
     
     const io = getIO()
-    catchSocketError(() => io.to(newNotification.recepientId)
+    catchSocketError(() => io.to(newNotification.recipientId)
     .emit(
         "new_notification", 
         newNotification
@@ -18,6 +19,18 @@ export const sendAndSaveNotifacation = async (data: INotification) => {
 }
 
 export const getNotifications = async (userId: Types.ObjectId) => {
-    const notifications = await Notification.find({ recepientId: userId.toString() });
-    return notifications;
+    const notifications = await Notification.find({ 
+        recipientId: userId.toString(),
+        createdAt : -1
+        })
+        .limit(30);
+    const usersMakeNotifications = [...notifications].map(n => n.userId);
+    const names = await User.find({_id : {$in : usersMakeNotifications}})
+    .select("name")
+    .lean()
+    return {
+        notifications: notifications,
+        count: notifications.length,
+        names: names
+    };
 };
---
+++ src/modules/notifications/notifictions.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express"
+import { authUser } from "../../middleware/auth.middleware"
+import { getNotificationsController } from "./notifications.controller"
+
+
+const router = Router()
+router.get("/",authUser,getNotificationsController)
