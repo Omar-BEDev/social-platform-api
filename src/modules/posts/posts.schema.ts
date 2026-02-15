dce17f8498892f1cd4ec23c53ea3af559ee6aca8
Omar <kaliochibrahim@gmail.com>
2026-02-15 23:48:57 +0000

    Update posts.schema.ts

--- src/modules/posts/posts.schema.ts
+++ src/modules/posts/posts.schema.ts
@@ -11,7 +11,6 @@ export const objectIdSchema = z.string().refine(
 );
 
 export const postSchema = z.object({
-  authorId: objectIdSchema,
   content: z.string().min(1).max(2000),
   languageTag: z.array(z.string()).min(1).max(6),
   frameworkTag: z.array(z.string()).max(3).optional(),
